8ffedaf9ab078328b1131eab8d0837b5205cecd5
njorocs <njorocs@gmail.com>
2023-10-05 12:32:26 +0300

    Updating missed appointment to use refill date and ART fast track visit in appointment & attrition report

--- api/src/main/java/org/openmrs/module/kenyaemrextras/reporting/library/continuityOfTreatment/AppointmentAttritionCohortLibrary.java
+++ api/src/main/java/org/openmrs/module/kenyaemrextras/reporting/library/continuityOfTreatment/AppointmentAttritionCohortLibrary.java
@@ -33,34 +33,34 @@ public class AppointmentAttritionCohortLibrary {
 	 */
 	public CohortDefinition totalAppointments() {
 		SqlCohortDefinition cd = new SqlCohortDefinition();
-		String sqlQuery = "select t.patient_id\n" +
-				"from (select fup.patient_id,\n" +
-				"             d.patient_id                                                    as disc_patient,\n" +
-				"             d.visit_date                                                    as disc_date,\n" +
-				"             mid(max(concat(fup.visit_date, fup.next_appointment_date)), 11) as latest_next_appointment_date,\n" +
-				"             greatest(mid(max(concat(fup.visit_date, fup.refill_date)), 11),\n" +
-				"                      ifnull(max(d.visit_date), '0000-00-00'))               as refill_tca\n" +
-				"      from kenyaemr_etl.etl_patient_hiv_followup fup\n" +
-				"               left join (select k.patient_id, max(k.visit_date) as latest_fast_track_visit_date\n" +
-				"                          from kenyaemr_etl.etl_art_fast_track k\n" +
-				"                          group by k.patient_id) k on fup.patient_id = k.patient_id\n" +
-				"               join kenyaemr_etl.etl_patient_demographics p on p.patient_id = fup.patient_id\n" +
-				"               join kenyaemr_etl.etl_hiv_enrollment e on fup.patient_id = e.patient_id\n" +
-				"               left outer JOIN\n" +
-				"           (select patient_id,\n" +
-				"                   coalesce(max(date(effective_discontinuation_date)), max(date(visit_date))) visit_date,\n" +
-				"                   max(date(effective_discontinuation_date)) as                               effective_disc_date,\n" +
-				"                   discontinuation_reason\n" +
-				"            from kenyaemr_etl.etl_patient_program_discontinuation\n" +
-				"            where date(visit_date) <= date(:endDate)\n" +
-				"              and program_name = 'HIV'\n" +
-				"            group by patient_id) d on d.patient_id = fup.patient_id\n" +
-				"      where fup.visit_date <= date(:endDate)\n" +
-				"        and fup.next_appointment_date between date(:startDate) AND date(:endDate)\n" +
-				"      group by patient_id\n" +
-				"      having latest_next_appointment_date between date(:startDate) AND date(:endDate)\n" +
-				"          or refill_tca between date(:startDate) AND date(:endDate)\n" +
-				"          AND (max(e.visit_date) >= date(disc_date) or disc_patient is null or disc_date >= date(:endDate))) t;";
+		String sqlQuery = "select t.patient_id\n"
+		        + "from (select fup.patient_id,\n"
+		        + "             d.patient_id                                                    as disc_patient,\n"
+		        + "             d.visit_date                                                    as disc_date,\n"
+		        + "             mid(max(concat(fup.visit_date, fup.next_appointment_date)), 11) as latest_next_appointment_date,\n"
+		        + "             greatest(mid(max(concat(fup.visit_date, fup.refill_date)), 11),\n"
+		        + "                      ifnull(max(d.visit_date), '0000-00-00'))               as refill_tca\n"
+		        + "      from kenyaemr_etl.etl_patient_hiv_followup fup\n"
+		        + "               left join (select k.patient_id, max(k.visit_date) as latest_fast_track_visit_date\n"
+		        + "                          from kenyaemr_etl.etl_art_fast_track k\n"
+		        + "                          group by k.patient_id) k on fup.patient_id = k.patient_id\n"
+		        + "               join kenyaemr_etl.etl_patient_demographics p on p.patient_id = fup.patient_id\n"
+		        + "               join kenyaemr_etl.etl_hiv_enrollment e on fup.patient_id = e.patient_id\n"
+		        + "               left outer JOIN\n"
+		        + "           (select patient_id,\n"
+		        + "                   coalesce(max(date(effective_discontinuation_date)), max(date(visit_date))) visit_date,\n"
+		        + "                   max(date(effective_discontinuation_date)) as                               effective_disc_date,\n"
+		        + "                   discontinuation_reason\n"
+		        + "            from kenyaemr_etl.etl_patient_program_discontinuation\n"
+		        + "            where date(visit_date) <= date(:endDate)\n"
+		        + "              and program_name = 'HIV'\n"
+		        + "            group by patient_id) d on d.patient_id = fup.patient_id\n"
+		        + "      where fup.visit_date <= date(:endDate)\n"
+		        + "        and fup.next_appointment_date between date(:startDate) AND date(:endDate) or fup.refill_date between date(:startDate) AND date(:endDate)\n"
+		        + "      group by patient_id\n"
+		        + "      having latest_next_appointment_date between date(:startDate) AND date(:endDate)\n"
+		        + "          or refill_tca between date(:startDate) AND date(:endDate)\n"
+		        + "          AND (max(e.visit_date) >= date(disc_date) or disc_patient is null or disc_date >= date(:endDate))) t;";
 		cd.setName("totalAppointments");
 		cd.addParameter(new Parameter("startDate", "Start Date", Date.class));
 		cd.addParameter(new Parameter("endDate", "End Date", Date.class));
